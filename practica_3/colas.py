4f479670a1e1e3467d8aa44fe1744583e15120df
Julio Aquino Ibarra <julio.aquinoi@uap.uaz.edu.mx>
2026-03-02 13:54:14 -0600

    [create] colas y terminal banco

---
+++ practica_3/colas.py
@@ -0,0 +1,33 @@
+def enque(lista, elemento):
+    lista.append(elemento)
+
+def deque(lista):
+    lista.pop(0)
+
+def peek(lista):
+    return lista[0]
+
+def is_empty(lista):
+    if lista == []:
+        return True
+    else:
+        return False
+
+def size(lista):
+    return len(lista)
+
+
+lista =[]
+
+# print(is_empty(lista))
+enque(lista,1)
+print(lista)
+print(is_empty(lista))
+deque(lista)
+print(is_empty(lista))
+enque(lista,8)
+enque(lista,131)
+deque(lista)
+enque(lista,0)
+
+print(lista)
---
+++ practica_3/terminal_banco.py
@@ -0,0 +1,69 @@
+def enque(lista, elemento):
+    lista.append(elemento)
+
+def deque(lista):
+    lista.pop(0)
+
+def peek(lista):
+    return lista[0]
+
+def is_empty(lista):
+    if lista == []:
+        return True
+    else:
+        return False
+
+def size(lista):
+    return len(lista)
+
+def retirosss(lista,lista2):
+    r = lista[0]-lista2[0]
+    deque(lista)
+    deque(lista2)
+    enque(lista,r)
+
+def retiro_w(lista,cant):
+    dinero = peek(lista)
+    deque(lista)
+    cant_re = cant
+    dinero = dinero-cant_re
+    enque(retiro,cant_re)
+    enque(saldos,dinero)
+
+
+def deposito_w(lista,cant):
+    dinero = peek(lista)
+    deque(lista)
+    cant_de = cant
+    dinero = dinero+cant_de
+    enque(deposito,cant_de)
+    enque(saldos,dinero)
+
+
+saldos = []
+retiro = []
+deposito = []
+
+enque(saldos,1000)
+enque(saldos,1000)
+enque(saldos,1000)
+enque(saldos,1000)
+enque(saldos,1000)
+
+retiro_w(saldos,500)
+retiro_w(saldos,500)
+retiro_w(saldos,500)
+retiro_w(saldos,500)
+retiro_w(saldos,500)
+
+deposito_w(saldos,300)
+deposito_w(saldos,300)
+deposito_w(saldos,300)
+deposito_w(saldos,300)
+deposito_w(saldos,300)
+
+# enque(retiro,500)
+# retirosss(saldos,retiro)
+print(saldos)
+print(retiro)
+print(deposito)
